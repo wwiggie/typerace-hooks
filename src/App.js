bfb8ee5ad4109565362f3d473c1fe4ac3e3639ea
Wendy Wigg <wiggwendy@gmail.com>
2019-09-10 09:06:20 -0700

    useState hook working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, /* hook */{ useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  // apply the useState hook and set up state functionality for userText. userText is the current value. setUserText updates the userText value.
+  const [userText, setUserText] = useState('');
+
+  // helper method that passes in the value from the user's input
+  const updateUserText = event => {
+    setUserText(event.target.value);
+    console.log('current userText', userText);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Type Race</h2>
+      <input value={userText} onChange={updateUserText} />
     </div>
-  );
+  )
 }
 
 export default App;
